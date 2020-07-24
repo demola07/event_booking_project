7e02d1760efb15bd3c64ea564c07cfcd5f2afbc2
Ademola Sobaki <demolasobaki@gmail.com>
2020-07-24 21:41:51 +0100

    create event model, also create and get all events resolver

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
-const { graphqlHTTP } = require('express-graphql')
-const { buildSchema } = require('graphql')
+const {
+  graphqlHTTP
+} = require('express-graphql')
+const {
+  buildSchema
+} = require('graphql')
 
 const connectDB = require('./config/db')
+
+const Event = require('./models/event')
 
 const app = express()
 
@@ -44,8 +50,33 @@ app.use(
       }
     `),
     rootValue: {
-      events: () => {},
-      createEvent: (args) => {}
+      events: () => {
+        return Event.find().then(result => {
+          console.log(result);
+          return result.map(event => {
+            return { ...event._doc }
+          })
+        }).catch(err => {
+          console.log(err);
+        })
+      },
+
+      createEvent: (args) => {
+        const event = new Event({
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: args.eventInput.price,
+          date: new Date(args.eventInput.date)
+        })
+
+        return event.save().then(result => {
+          console.log(result);
+          return { ...result._doc }
+        }).catch(err => {
+          console.log(err)
+          throw err;
+        })
+      }
     },
     graphiql: true
   })
